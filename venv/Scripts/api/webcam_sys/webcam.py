f606a5100e9efd478948396dab6b31edc91e7a93
David <skosanadavid67@gmail.com>
2026-07-18 01:24:53 +0200

    webcam_sys update 1

--- venv/Scripts/api/webcam_sys/webcam.py
+++ venv/Scripts/api/webcam_sys/webcam.py
@@ -1,4 +1,30 @@
+import platform
 
+from aiortc.contrib.media import MediaRelay
 
 class Webcam:
-    pass
+    def __init__(self):
+        self.option = {"video_size": "1280x720", 
+                       "framerate": "30"}
+        self.relay = MediaRelay()
+        self.webcam = None
+        
+    @property    
+    def os_available(self):
+        """
+        FUNCTION THAT DETECT THE OPERATING SYSTEM, 
+        THAT IS USING THE APPLICATION.
+        (e.g windows, linux or ios)
+        """
+        os_name = platform.system()
+        return os_name
+    
+    def tracks(self):
+        return None, self.relay.subscribe(self.webcam.video)
+            
+
+c = Webcam()
+print(c.os_available)    
+
+        
+    
