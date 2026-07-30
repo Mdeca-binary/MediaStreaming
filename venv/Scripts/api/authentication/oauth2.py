1d14a4412832412fc2ac80cd23cebbf721aed638
David <skosanadavid67@gmail.com>
2026-07-30 19:10:27 +0200

    auth2

--- venv/Scripts/api/authentication/oauth2.py
+++ venv/Scripts/api/authentication/oauth2.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from typing import LiteralString
 
 from passlib.context import CryptContext
@@ -9,35 +11,56 @@ from fastapi.security import (
 )
 
 from models.model import UserModel
+from models.schemas import UserBaseMode
 from models.database import session
 
-class Authentication:
+class Authentication():
     
-    def __init__(self, username:LiteralString, password:LiteralString):
+    def __init__(self, username:str|None, 
+                 unhashed_password:str|None):
         self.username = username
-        # THIS IS A PLAIN TEXT FROM USER-INPUT
-        self.password = password
-        self.hash_type = CryptContext(schemes=["bcrypt"], 
-                                      deprecated="auto")
-        self.user_database = UserModel
-    
-    @property
-    def encrypt_user_password(self):
-        return self.hash_type.hash(self.password)
-    
-    def verify_user_password(self, plain_password: LiteralString):
-        return self.hash_type.verify(plain_password, self.encrypt_user_password)
-    
-    def check_username_exists(self, username:str):
-        return username in \
-            session.query(self.user_database).filter_by(username=username)
-                
-    def save_user_data(self):
-        user = UserModel(
-            username=self.username, 
-            password=self.encrypt_user_password
+        self.unhashed_password = unhashed_password
+        self.password_context = CryptContext(
+            schemes=["bcrypt", "pbkdf2_sha256"],
+            deprecated=["md5_crypt"]
         )
-        with session:
-            session.add(user)
-            session.commit()
-            session.refresh(user)
+        # self.db = UserModel()
+
+    def encrypt_user_password(self, unhashed_pwd:str):
+        # THIS METHOD HERE HASH'S  THE USER PASSWORD. 
+        return \
+            self.password_context.hash(unhashed_pwd)
+
+    def decrypt_user_password(self, encrypted_password):
+        # THIS METHOD CHECKS IF THE UNHASHED PASSWORD MATCHED
+        # THE ENCRYPTED PASSWORD IS THE SAME AS.
+        return self.password_context.verify(self.unhashed_password, encrypted_password)
+    
+    def user_data(self):
+        usr = {
+            "%s"%(self.username): {
+                "username": self.username, 
+                "hashed_pwd": self.encrypt_user_password(unhashed_pwd=self.unhashed_password),  
+                "account_created": datetime.now(),
+                "update_visit": datetime.now()
+            }
+        }
+        if self.username in usr:
+            user_dict = usr[self.username]
+        return UserBaseMode(**user_dict)
+         
+    
+    def save_user_in_database(self):
+        if self.username is not None:
+            if self.username not in session.query(UserModel).filter_by(username=self.username):
+                _password = self.encrypt_user_password(self.unhashed_password)
+                usr = UserModel(
+                    username=self.username, 
+                    password=_password,
+                    account_created=datetime.datetime.now(),
+                    update_visit = datetime.datetime.now()
+                )
+                session.add(usr)
+                session.commit()
+        
+        
--- venv/Scripts/api/configurations/config.py
+++ venv/Scripts/api/configurations/config.py
@@ -1,5 +1,5 @@
 import os
-
+from fastapi.security import OAuth2PasswordBearer
 class StaticConfig:
     # CONFIGURATIONS FOR CSS AND JAVASCRIPT
     # CURRENT FOLDER
@@ -38,4 +38,15 @@ DATABASE_URL = {
 
 class APIConfig:
     SECRET_KEY = ""
-    
+
+OAUTH2_SCHEME = OAuth2PasswordBearer(
+    tokenUrl="token", 
+    scopes={"user": "",
+            "information": ""}
+)
+
+class JWTConfig:
+    PRIVATE_KEY = b""
+    PUBLIC_KEY = b""
+    ALGORITHM = "PS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = 30 
